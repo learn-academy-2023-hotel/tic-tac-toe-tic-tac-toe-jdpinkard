2022464041b871c73a65f9867cd2e6715a83dde0
Justin Pinkard <justin.d.pinkard@gmail.com>
2024-01-01 05:40:17 -0600

    Skeleton Done

---
+++ src/components/ResetButton/ResetButton.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './ResetButton.css'
+
+const ResetButton = ({ resetBoard }) => {
+  return ( 
+  <div>
+    <button className='reset-btn' onClick={resetBoard}>Restart</button>
+  </div>
+  )
+}
+
+export default ResetButton
